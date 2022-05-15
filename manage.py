abcec1a4cc9e1979e6d421870f45b40e9b03612d
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 23:08:07 +0300

    create shell

--- manage.py
+++ manage.py
@@ -1,10 +1,26 @@
-from multiprocessing import Manager
-from app import create_app
-
+from flask_script import Manager,Server
+from app import create_app,db
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Article
 
 
 app = create_app("development")
 
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=5).run(tests)
+@manager.shell
+def make_shell_context():
+    return {'db' : db,'User':User , 'Article':Article}
 
 
 

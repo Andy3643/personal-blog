0dd083e47ccabd9364c50f3a4bc03ff64a849eb4
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 16:05:21 +0300

    create authentication blueprint

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,17 +1,3 @@
-import email
-from flask_wtf import FlaskForm
-from wtforms.validators import EqualTo,DataRequired,ValidationError,Email
-from wtforms import StringField,SubmitField,TextAreaField,BooleanField,PasswordField
-
-class LoginForm (FlaskForm):
-    email = StringField('Please enter your Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Please enter your Password',validators=[DataRequired()])
-    remember = BooleanField('remember me')
-    submit = SubmitField('Log In')
-    
-class RegistrationForm(FlaskForm):
-    username = StringField('Enter your Username',validators=[DataRequired()])
-    email = StringField('Please Enter Your Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Please Enter Your Password',validators=[DataRequired()])
-    confirm_password = PasswordField('Please Retype Your Password',validators=[DataRequired(),EqualTo('confirm_password',message='Passwords do not match !')])
-    submit = SubmitField('Sign Up')
+from flask import Blueprint
+auth = Blueprint('auth',__name__)
+from . import views,forms

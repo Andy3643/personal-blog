c37940064f2601ad8b50c6cfe5f93511752d6afc
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 20:02:45 +0300

    update user profile

--- app/main/views.py
+++ app/main/views.py
@@ -24,7 +24,7 @@ def add_article():
     return render_template('newblog.html',title = title,articleform = form)
 
 
-@main.route('/articlediscussion/<int:article_id>/comment',methods=['POST','GET'])
+@main.route('/blogdiscussion/<int:article_id>/comment',methods=['POST','GET'])
 @login_required
 def comment(article_id):
     current_article = Article.query.filter_by(id = article_id).first()
@@ -51,3 +51,56 @@ def profile(username):
         abort(404)
 
     return render_template('profile/profile.html',user = user,my_articles = my_articles)
+
+
+@main.route('/user/<username>/update', methods = ['GET','POST'])
+@login_required
+def profle_update(username):
+    user = User.query.filter_by(username = username).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile',username = user.username))
+
+    return render_template('profile/update.html',form= form)
+
+@main.route('/user/<username>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(username):
+    user = User.query.filter_by(username = username).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',username=username))
+
+@main.route('/blogdiscussion/<int:article_id>/update',methods=['POST','GET'])
+@login_required
+def update(article_id):
+    current_article = Article.query.filter_by(id = article_id).first()
+    if current_article.user != current_user:
+        abort(403)
+    form = UploadBlogForm()
+    if form.validate_on_submit():
+        current_article.article = form.article.data
+        current_article.category = form.category.data
+        db.session.commit()
+        return redirect('.comment',article_id = article_id)
+    elif request.method == 'GET':
+        form.category.data = current_article.category
+        form.article.data = current_article.article
+    return render_template('addarticle.html',title = 'update article',articleform = form)
+
+
+@main.route('/blogdiscussion/<int:article_id>/delete',methods=['POST'])
+@login_required
+def delete(article_id):
+    current_article = Article.query.filter_by(id = article_id).first()
+    if current_article.user != current_user:
+        abort(403)
+    db.session.delete(current_article)
+    db.session.commit()
+    return redirect(url_for('.index'))

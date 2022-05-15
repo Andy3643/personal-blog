dbd583207d1723cb8341e3f227941421b8353781
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 22:28:38 +0300

    add user class

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    '''
+    data base for users
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String,unique = True,nullable = False)
+    email = db.Column(db.String,unique = True,nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    article = db.relationship('Article',backref='user',lazy = 'dynamic')
+    comments = db.relationship('Comment',backref='user',lazy='dynamic')
+    password_hash = db.Column(db.String,nullable=False)
+    

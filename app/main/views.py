2ad551ddc8ca5143607accd7da471eeb5edbdf80
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 19:06:31 +0300

    add route to display user commens on blog

--- app/main/views.py
+++ app/main/views.py
@@ -22,3 +22,19 @@ def add_article():
         return redirect(url_for('.index'))
     title = 'Add a article'
     return render_template('newblog.html',title = title,articleform = form)
+
+
+@main.route('/articlediscussion/<int:article_id>/comment',methods=['POST','GET'])
+@login_required
+def comment(article_id):
+    current_article = Article.query.filter_by(id = article_id).first()
+    if request.method == "POST":
+        comment = request.form.get("comment")
+        new_comment = Comment(comment = comment,user = current_user,article = current_article)
+        db.session.add(new_comment)
+        db.session.commit()
+    article = Article.query.get_or_404(article_id)
+    comments = Comment.get_comments(article_id)
+
+    title = 'Article Discussion'
+    return render_template('blogdiscussion.html',title = title,article = article,comments=comments)

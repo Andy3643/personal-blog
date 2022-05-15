851635e2f074953182c1e0912c8e6bacd4ff5557
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-15 16:42:30 +0300

    registering blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,12 @@ def create_app(configname):
     bootstrap.init_app(app)
     
     
-    
+    #registering blueprints
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+
     
     
     
